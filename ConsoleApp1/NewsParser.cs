91858c83c130db9e1ce96ece7315c626eec2e037
Myrsheftor <mv131205@gmail.com>
2021-06-17 19:24:20 +0300

    Initial commit

---
+++ ConsoleApp1/NewsParser.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AngleSharp;
+
+namespace Awesome
+{
+    public static class NewsParser
+    {
+        public static async Task<string> FindNews(string text)
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+
+            var uri = $"https://dtf.ru/search/v2/content/new?query={text}";
+            var document = await context.OpenAsync(uri);
+            var selector = "div.content-title.content-title--short.l-island-a";
+            var elements = document.QuerySelectorAll(selector);
+            var arr = elements.Select(it => it.TextContent);
+            var result = string.Join("\n", arr);
+           
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace Awesome {
+  class Program {
+    static ITelegramBotClient botClient;
+
+    static void Main() {
+      botClient = new TelegramBotClient("1877165584:AAEHhv4mvxLtawpOOBtSYBQbZZPtxxqPI3k");
+
+      var me = botClient.GetMeAsync().Result;
+      Console.WriteLine(
+        $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+      );
+
+      botClient.OnMessage += Bot_OnMessage;
+      botClient.StartReceiving();
+
+      Console.WriteLine("Press any key to exit");
+      Console.ReadKey();
+
+      botClient.StopReceiving();
+    }
+
+    static async void Bot_OnMessage(object sender, MessageEventArgs e) {
+      if (e.Message.Text != null)
+      {
+        Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+        var newsMess = await NewsParser.FindNews(e.Message.Text);
+        await botClient.SendTextMessageAsync(
+          chatId: e.Message.Chat,
+          text:   newsMess
+        );
+      }
+    }
+  }
+}
